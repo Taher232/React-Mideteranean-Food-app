52554e5504ce4739499b6092cc5cb63c59a80751
taher.chabaane <taher.chabaane.1@gmail.com>
2022-05-14 17:33:53 +0200

    Mideteranean food

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assests/logo.jpg";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={logo} />
+      </div>
+      <div className="rightSide"> right side</div>
+    </div>
+  );
+}
+
+export default Navbar;
