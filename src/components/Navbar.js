d35a95c59b9582e63e19ee42ffce336ee8cd97e1
taher.chabaane <taher.chabaane.1@gmail.com>
2022-05-15 18:11:52 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from "react";
-import logo2 from "../assests/logo2.jpg";
+import logo5 from "../assests/logo5.jpg";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
-        <img src={logo2} />
+        <img src={logo5} />
+        <div className="hiddenLinks"></div>
       </div>
-      <div className="rightSide"> right side</div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/">Menu</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Contacts</Link>
+      </div>
     </div>
   );
 }
